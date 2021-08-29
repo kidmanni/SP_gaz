32cc25ef645751b90816b0db4232bbee65f832e8
azzerino <azzer.4@gmail.com>
2021-08-29 17:35:57 +0200

    Adding the response navbar and the animation for the car

--- js/app.js
+++ js/app.js
@@ -8,16 +8,24 @@ function handleParallax() {
 		factorXl: 0, // - to +
 		type: 'background', // foreground
 		direction: 'vertical', // horizontal
-		transition: 'translate 0.1s ease'
+		transition: 'translate 0.01s ease'
 	});
 }
-function handleMenu(){
-	$(".menu-icon").click(function(){
+function handleMenu() {
+	$(".menu-icon").click(function () {
+		let $this = $(this)
+		if ($(".menu-nav").hasClass("hidden")) {
+			let closeIcon = $this.attr("src").replace(/menu/gi, "Menu_close")
+			$this.attr("src", closeIcon)
+		} else {
+			let menuIcon = $this.attr("src").replace(/Menu_close/gi, "Menu")
+			$this.attr("src", menuIcon)
+		}
 		$(".menu-nav").fadeToggle(500)
 		$(".menu-nav").toggleClass("hidden")
 	})
 }
-function main(){
+function main() {
 	handleMenu()
 	handleParallax()
 }
