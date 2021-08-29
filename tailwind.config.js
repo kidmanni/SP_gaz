55a330fa02e70752b3399df496ee5f83f837b485
Anna Leyn <annaleyn.twitch@gmail.com>
2021-08-29 13:19:46 +0300

    first push

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        "blue-custom": "#008BD2",
+        "yellow-custom": "#F9B02D",
+        "red-custom": "#E7332F",
+        "green-custom": "#3AAA35",
+        "black-custom": "#29282A",
+        "gray-custom": "#949296",
+        "light-gray-custom": "#E9E7EB"
+
+      },
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
