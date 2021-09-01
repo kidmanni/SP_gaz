49ed772bf26ee653834de5b2690382dce74b2fda
azzerino <azzer.4@gmail.com>
2021-09-01 13:23:51 +0200

    Adding the new parallax speed and observer to capture when the element is visible, and adding some styles for dropdown

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 function handleParallax() {
-	$(".parallax-container").paroller({
-		factor: 0, // - to +
-		factorXs: 0, // - to +
-		factorSm: 0, // - to +
-		factorMd: 0, // - to +
-		factorLg: 0, // - to +
-		factorXl: 0, // - to +
-		type: 'background', // foreground
-		direction: 'vertical', // horizontal
-		transition: 'translate 0.01s ease'
-	});
+	// $(".parallax-container").paroller({
+	// 	factor: 0, // - to +
+	// 	factorXs: 0, // - to +
+	// 	factorSm: 0, // - to +
+	// 	factorMd: 0, // - to +
+	// 	factorLg: 0, // - to +
+	// 	factorXl: 0, // - to +
+	// 	type: 'background', // foreground
+	// 	direction: 'vertical', // horizontal
+	// 	transition: 'translate 0.01s ease'
+	// });
+	$(".parallax-container").paroller()
 }
 function handleMenu() {
 	$(".menu-icon").click(function () {
@@ -25,9 +26,21 @@ function handleMenu() {
 		$(".menu-nav").toggleClass("hidden")
 	})
 }
+function elementInViewport(el) {
+	var observer = new IntersectionObserver(function (entries) {
+		// isIntersecting is true when element and viewport are overlapping
+		// isIntersecting is false when element and viewport don't overlap
+		if (entries[0].isIntersecting === true) {
+			console.log('Element has just become visible in screen');
+			handleParallax()
+		}
+	}, { threshold: [0] });
+
+	observer.observe(document.querySelector(el));
+}
 function main() {
 	handleMenu()
-	handleParallax()
+	elementInViewport(".parallax-container img")
 }
 
 // =================================================
